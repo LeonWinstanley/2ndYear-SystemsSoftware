35cd22bda863eb578638d51bbe5830ade6f02c79
Adam <t7.shab@gmail.com>
2019-03-05 11:22:47 +0000

    Uploaded src

---
+++ src/Client.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+// below are Jframe imports
+import java.awt.GraphicsConfiguration;
+import javax.swing.JFrame;
+
+public class Client{
+
+    BufferedReader ClientInput, ClientOutput;
+    PrintWriter OutputToServer;
+    Socket socket;
+    Thread t1, t2;
+    String in = "", out = "", serverThread = "", clientThread = "";
+    static GraphicsConfiguration gc;
+
+    public Client() {
+        while (true) {
+
+            try {
+                //t1 = new Thread(this, serverThread);
+                //t2 = new Thread(this, clientThread);
+                socket = new Socket("192.168.0.1", 50000);
+                t1.start();
+                t2.start();
+
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public void run() {
+
+        try {
+            if (Thread.currentThread() == t2) {
+                do {
+                    ClientInput = new BufferedReader(new InputStreamReader(System.in));
+                    OutputToServer = new PrintWriter(socket.getOutputStream(), true);
+                    in = ClientInput.readLine();
+                    OutputToServer.println(in);
+                } while (!in.equals("END"));
+            } else {
+                do {
+                    ClientOutput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    out = ClientOutput.readLine();
+                    System.out.println("Server says : : : " + out);
+                } while (!out.equals("END"));
+            }
+        } catch (Exception e) {
+        }
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(gc);
+        frame.setSize(700, 500);
+
+        frame.setTitle("Client GUI");
+
+        
+        frame.setVisible(true);
+        new Client();
+    }
+}
---
+++ src/WeatherClient.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class WeatherClient{
+
+    PrintWriter OutputToServer;
+    Socket socket;
+    Thread t1, t2;
+    String serverThread = "", clientThread = "";
+
+    public WeatherClient() {
+        while (true) {
+            try {
+
+                //t1 = new Thread(this, serverThread);
+                //t2 = new Thread(this, clientThread);
+                socket = new Socket("192.168.0.1", 50000);
+                t1.start();
+                t2.start();
+
+            } 
+            catch (Exception e) {
+
+            }
+        }
+    }
+
+    public void run() {
+        // Generate GPS, Humidity, Temperature and other data here using random functions.
+    }
+
+    public static void main(String[] args) {
+        
+        new WeatherClient();
+
+    }
+}
