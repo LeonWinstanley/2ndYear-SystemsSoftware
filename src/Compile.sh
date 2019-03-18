6d92cacd1279dcf8e194e1bb83b5df3e2f5c6e27
Adam Sheppard <t7.shab@gmail.com>
2019-03-18 12:12:59 +0000

    Updated script to either clean or make the java files

--- src/Compile.sh
+++ src/Compile.sh
@@ -1,6 +1,25 @@
-javac Client.java
-javac Hash.java
-javac Login.java
-javac LoginTest.java
-javac Server.java
-javac WeatherClient.java
+#!/bin/bash 
+
+make ()
+{
+    javac *.java
+    echo Built successfully.
+}
+
+clean ()
+{
+    rm *.class
+    echo Cleaned successfully.   
+}
+
+echo clean or make
+
+read input
+
+if [ "$input" = "clean" ]
+then
+    clean
+elif [ "$input" = "make" ]
+then
+    make
+fi
