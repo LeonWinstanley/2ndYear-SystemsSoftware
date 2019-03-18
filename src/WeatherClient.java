22bbba861880401f0ba06b156203b5e758538838
Matthew Ball <N0743797@my.ntu.ac.uk>
2019-03-18 12:58:25 +0000

    Update Weather Client

--- src/WeatherClient.java
+++ src/WeatherClient.java
@@ -26,13 +26,40 @@ public class WeatherClient{
         }
     }
 
+    // Uses Lat Long System
+    public Double GPS() {
+        double minimum = -90;
+        double maximum = 90;
+        double randomDouble = new Random().nextDouble();
+        double result = minimum + (randomDouble * (maximum - minimum));
+        GPSData = result;
+        return GPSData;
+    }
+
+
+    public int Humidity() {
+        int HumidityData;
+        HumidityData = 1;
+        return HumidityData;
+    }
+
+    // Uses Degrees Centigrade
+    public int Temperature() {
+        int Temperature;
+        Temperature = 20;
+        return Temperature;
+    }
+
     public void run() {
         // Generate GPS, Humidity, Temperature and other data here using random functions.
+        System.out.println("Test");
     }
 
     public static void main(String[] args) {
         
         new WeatherClient();
-
+        //Pair <Double, Double> epic = new GPS();
+        //System.out.println(epic);
+        System.out.println("Test");
     }
 }
