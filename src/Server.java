166a2d9b1ad14f25a2647674d44d06676ebb5e66
Adam Sheppard <t7.shab@gmail.com>
2019-03-25 10:53:48 +0000

    Using the proper Server file now

--- src/Server.java
+++ src/Server.java
@@ -1,154 +1,122 @@
 import java.io.*;
-import java.net.*;
 import java.util.*;
-// below are Jframe imports
-import java.awt.*;
-import javax.swing.*;
+import java.net.*;
 
+// Server class 
 public class Server {
 
-    static ServerSocket weatherSocket, userSocket;
-    static ArrayList<Socket> userList = new ArrayList<Socket>();
-    static ArrayList<Socket> weatherList = new ArrayList<Socket>();
-    static ArrayList<Thread> threadList = new ArrayList<Thread>();
-    static GraphicsConfiguration gc;
+    // Vector to store active clients
+    static Vector<ClientHandler> ar = new Vector<>();
 
-    // BufferedReader ServerInput, ServerOutput;
-    // PrintWriter OutputToClient;
-    // Socket socket;
-    // Thread recieveingThread, sendingThread, listenThread;
-    // String in = "", out = "";
+    // counter for clients
+    static int i = 0;
 
-    public Server() {
-        try {
+    public static void main(String[] args) throws IOException {
+        // server is listening on port 1234
+        ServerSocket ss = new ServerSocket(1234);
 
-            // recieveingThread = new Thread(new WeatherClient());
-            // sendingThread = new Thread(new UserClient());
-            weatherSocket = new ServerSocket(50000);
-            userSocket = new ServerSocket(50001);
+        Socket s;
 
-            threadList.add(new Thread(new Listen()));
-
-            threadList.get(0).start();
-
-            threadList.get(1).start();
-
-            // System.out.println("Client connected with Ip " +
-            // socket.getInetAddress().getHostAddress());
-            // t1.start();
-            // t2.start();
-
-        } catch (Exception e) {
-        }
-    }
-
-    public ArrayList getUserList() {
-
-        return userList;
-    }
-
-    public static void setUserList(Socket input) {
-        userList.add(input);
-    }
-
-    public ArrayList getWeatherList() {
-
-        return weatherList;
-    }
-
-    public static void setWeatherList(Socket input) {
-        weatherList.add(input);
-    }
-
-    public ServerSocket getWeatherSocket() {
-        return weatherSocket;
-    }
-
-    public void setWeatherSocket(ServerSocket input) {
-        weatherSocket = input;
-    }
-
-    // public void run() {
-    // try {
-    // if (Thread.currentThread() == t1) {
-    // } else {
-    // do {
-    // ServerOutput = new BufferedReader(new
-    // InputStreamReader(socket.getInputStream()));
-    // out = ServerOutput.readLine();
-    // System.out.println("Client says : : : " + out);
-    // } while (!out.equals("END"));
-    // }
-    // } catch (Exception e) {
-    // }
-    // }
-
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame(gc);
-        // frame.setVisable(true);
-
-        new Server();
-
-    }
-}
-
-class WeatherClientThread implements Runnable {
-
-    @Override
-    public void run() {
-
-        System.out.println(" New Server Data");
-
-    }
-
-}
-
-class UserClientThread implements Runnable { // user could call function to pull data from server
-
-    Socket UserSocket;
-
-    @Override
-    public void run() {
-
-        System.out.println(" New Client Connected");
-
-        // do {
-        // ClientInput = new BufferedReader(new InputStreamReader(System.in));
-        // OutputToServer = new PrintWriter(socket.getOutputStream(), true);
-        // in = ClientInput.readLine();
-        // OutputToServer.println(in);
-        // } while (!in.equals("END"));
-
-    }
-
-}
-
-class Listen implements Runnable {
-
-    @Override
-    public void run() {
+        // running infinite loop for getting
+        // client request
         while (true) {
-            ServerSocket weathersocket = Server.weatherSocket;
-            try {
-                Server.setWeatherList(weathersocket.accept());
-                Server.threadList.add(new Thread(new WeatherClientThread())); // "Weather" +
-                // Integer.toString(Server.weatherList.size()))));
+            // Accept the incoming request
+            s = ss.accept();
 
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
+            System.out.println("New client request received : " + s);
 
-            ServerSocket usersocket = Server.userSocket;
-            try {
-                Server.setUserList(usersocket.accept());
-                Server.threadList.add(new Thread(new UserClientThread()));
+            // obtain input and output streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
 
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
+            System.out.println("Creating a new handler for this client...");
+
+            // Create a new handler object for handling this request.
+            ClientHandler mtch = new ClientHandler(s, "client " + i, dis, dos);
+
+            // Create a new Thread with this object.
+            Thread t = new Thread(mtch);
+
+            System.out.println("Adding this client to active client list");
+
+            // add this client to active clients list
+            ar.add(mtch);
+
+            // start the thread.
+            t.start();
+
+            // increment i for new client.
+            // i is used for naming only, and can be replaced
+            // by any naming scheme
+            i++;
 
         }
     }
+}
 
+// ClientHandler class
+class ClientHandler implements Runnable {
+    Scanner scn = new Scanner(System.in);
+    private String name;
+    final DataInputStream dis;
+    final DataOutputStream dos;
+    Socket s;
+    boolean isloggedin;
+
+    // constructor
+    public ClientHandler(Socket s, String name, DataInputStream dis, DataOutputStream dos) {
+        this.dis = dis;
+        this.dos = dos;
+        this.name = name;
+        this.s = s;
+        this.isloggedin = true;
+    }
+
+    @Override
+    public void run() {
+
+        String received;
+        while (true) {
+            try {
+                // receive the string
+                received = dis.readUTF();
+
+                System.out.println(received);
+
+                if (received.equals("logout")) {
+                    this.isloggedin = false;
+                    this.s.close();
+                    break;
+                }
+
+                // break the string into message and recipient part
+                StringTokenizer st = new StringTokenizer(received, "#");
+                String MsgToSend = st.nextToken();
+                String recipient = st.nextToken();
+
+                // search for the recipient in the connected devices list.
+                // ar is the vector storing client of active users
+                for (ClientHandler mc : Server.ar) {
+                    // if the recipient is found, write on its
+                    // output stream
+                    if (mc.name.equals(recipient) && mc.isloggedin == true) {
+                        mc.dos.writeUTF(this.name + " : " + MsgToSend);
+                        break;
+                    }
+                }
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+        }
+        try {
+            // closing resources
+            this.dis.close();
+            this.dos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
